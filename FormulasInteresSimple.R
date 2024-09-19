21b53c55e1ef3730788a2c3e4cc7908e77d0223f
jaba67 <2350483j@umich.mx>
2024-09-19 12:40:52 -0600

    Add files via upload
    se sube la primera carga de la tarea de interés simple

---
+++ FormulasInteresSimple.R
@@ -0,0 +1,12 @@
+# se presentan las funciones de interes simple.
+# Autor: Jesús Adriel Bravo Aguilar
+# V.1.0: 19 de Septiembre de 2024.
+
+# Valor futuro con interes simple:
+
+valorfinalSimple=function(VA,r,t){
+  xSalida=VA*(1+(r*t))
+  
+
+ return(xSalida) 
+}
